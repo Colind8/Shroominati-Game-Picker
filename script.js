09dc1ffdfbae8480e4370353474b59fa47e5d69d
Colind8 <colind8.awesome@gmail.com>
2021-02-21 08:46:24 +0000

    Fixed spelling error (missing period) for system shock's description

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function reroll() {
 
             if (systemshock == 1) {
                 document.getElementById("link").innerHTML = "System Shock: Enhanced Edition";
-                document.getElementById("desc").innerHTML = "Re-discover what made System Shock the first of it's kind, a ground breaking immersive sim that would inspire a generation of games to come";
+                document.getElementById("desc").innerHTML = "Re-discover what made System Shock the first of it's kind, a ground breaking immersive sim that would inspire a generation of games to come.";
                 document.getElementById("link").setAttribute("href","https://store.steampowered.com/app/410710/System_Shock_Enhanced_Edition/");
                 document.getElementById("reroll").innerHTML = "It's not even multiplayer";
             } else {
